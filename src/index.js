1b91f3a10376fce8bd425223721d1439dad6bc0c
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-05-17 16:37:25 -0400

    add function to display to-dos in console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import createToDo from "./to-do/create-todo";
+import displayToDo from "./to-do/display-todo";
 
 let testObject = createToDo('Test', 'Test Object', '05/15/22', 'High', 'Just a test object', 'yes');
-console.log(testObject);
+displayToDo(testObject.title, testObject.desc, testObject.dueDate, testObject.priority, testObject.notes, testObject.checklist);
