c06d18aad1538eb7bff7191e3bbe15a12beaa3b9
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-05-18 23:17:26 -0400

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import createToDo from "./create-todo";
 import displayToDo from "./display-todo";
 import createProject from "./create-project";
+// create an array for each project created
+const projects = [];
 
-let testObject = createToDo('Test', 'Test Object', '05/15/22', 'High', 'Just a test object', 'yes');
-displayToDo(testObject.title, testObject.desc, testObject.dueDate, testObject.priority, testObject.notes, testObject.checklist);
+let testObject = createToDo('Test', 'Test Task', '05/15/22', 'High', 'Just a test object', 'yes');
+
+displayToDo(
+    testObject.title,
+    testObject.desc,
+    testObject.dueDate,
+    testObject.priority,
+    testObject.notes,
+    testObject.checklist
+);
+
 let testProject = createProject('Default', 'Default Project');
-console.log(testProject);
+// add the test project to the projects array
+projects.push(testProject);
+testProject.tasks.push(testObject);
+console.log(projects);
