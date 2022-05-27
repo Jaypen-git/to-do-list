3e5a463bae8d335bb244950b22c908ec832d9093
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-05-27 15:20:54 -0400

    creates Project class

---
+++ src/project.js
@@ -0,0 +1,10 @@
+class Project {
+    constructor (title) {
+        this.title = title,
+        this.tasks = []
+    }
+    // This is where the tasks should be shown because each project has their own tasks
+    displayTasks () {
+        console.log(this.tasks);
+    }
+}
