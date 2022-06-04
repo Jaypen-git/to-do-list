69451b6293625ab80e5908263108cf71ea2d3e4f
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-06-04 14:54:50 -0400

    adds functions to add buttons for add forms

--- src/View.js
+++ src/View.js
@@ -1,38 +1,47 @@
 const projectContainer = document.querySelector('#project');
+const projectListContainer = document.querySelector('#projectList');
 
 const viewProject = (title) => {
-    // let createTaskTitle = () => {
-    //     let h3 = document.createElement('h3');
-    //     h3.innerText = title;
-    //     return h3;
-    // }
-    // // create a div for each task
-    // let createTaskDiv = () => {
-    //     let div = document.createElement('div');
-    //     div.appendChild(createTaskTitle());
-    //     projectContainer.appendChild(div);
-    // }
-    // tasklist.forEach(createTaskDiv());
-const createButton = (title) => {
-    let button = document.createElement('button');
-    button.innerText = title;
-    button.classList.add(title);
-    return button;
-}
+    const createButton = (title) => {
+        let button = document.createElement('button');
+        button.innerText = title;
+        button.classList.add(title);
+        return button;
+    }
 
     const createH1 = () => {
         let h1 = document.createElement('h1');
         h1.innerText = title;
         return h1;
-    };
-    const createDiv = () => {
+    }
+    const createDiv = (() => {
         let div = document.createElement('div');
         div.appendChild(createH1());
         div.appendChild(createButton('Edit'));
         div.appendChild(createButton('Delete'));
         projectContainer.appendChild(div);
-    }
-    createDiv();
+    })();
 }
 
-export default viewProject;
+const viewProjectList = (title) => {
+    let a = document.createElement('a');
+    a.innerText = title;
+    a.setAttribute('href', '#');
+    projectListContainer.appendChild(a);
+}
+
+const viewForm = (parent) => {
+    let form = document.createElement('form');
+    let input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    form.appendChild(input);
+    parent.appendChild(form);
+}
+
+const viewAddButton = (parent, item) => {
+    let a = document.createElement('a');
+    a.innerText = `Add ${item}`;
+    parent.appendChild(a);
+}
+
+export { viewProject, viewProjectList, viewAddButton };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { Task, Project } from './Constructors';
-import viewProject from './View';
+import { viewProject, viewProjectList, viewAddButton } from './View';
+
+const projectContainer = document.querySelector('#project');
+const projectListContainer = document.querySelector('#projectList');
 
 let Test = new Task ('Test', 'Just a test task', '05/26/22', 'High');
 let testProject = new Project ('Default');
-let testArray = ['Number One'];
+let testArray = [{title: 'Number One', key: 1}, {title: 'Number Two', key: 2}];
 
 // The created task should be added to the test project's tasks list
 testProject.tasks.push(Test);
 testProject.tasks.forEach(task => viewProject(task.title));
+testArray.forEach(project => viewProjectList(project.title));
+viewAddButton(projectContainer, 'Task');
+viewAddButton(projectListContainer, 'Project');
